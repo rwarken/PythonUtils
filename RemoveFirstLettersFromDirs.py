35ee4e4d078eacc93ce43294800e397d120ba44e
roni warken <rwarken@gmail.com>
2019-10-06 21:45:17 +0200

    Update RemoveFirstLettersFromDirs.py

--- RemoveFirstLettersFromDirs.py
+++ RemoveFirstLettersFromDirs.py
@@ -1,7 +1,7 @@
 import os
 
 # Define a folder to iterate through the files recursively
-folder_path = 'E:/Studying/Python Projects/PythonUtils/XML-files/'
+folder_path = 'dir_path'
 
 
 def replace_first_letters(folder_path, text, text_size):

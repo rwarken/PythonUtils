5fb38204fdc7337572ebe822a5356888df32c9f3
roni warken <rwarken@gmail.com>
2019-10-06 21:19:49 +0200

    Update FileSpacesToUnderscores.py

--- FileSpacesToUnderscores.py
+++ FileSpacesToUnderscores.py
@@ -7,7 +7,6 @@ folder_path = 'my_local_path/'
 def emptySpaces(folder_path):
     for path, subdirs, files in os.walk(folder_path):
         for file_name in files:
-            print(file_name)
             file_path = os.path.join(path, file_name)
             new_name = os.path.join(path, file_name.replace(' ', '_'))
             os.rename(file_path, new_name)

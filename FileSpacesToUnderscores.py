75e79225d279142fce73db18a869f88141915e39
roni warken <rwarken@gmail.com>
2019-10-06 21:44:38 +0200

    Update FileSpacesToUnderscores.py

--- FileSpacesToUnderscores.py
+++ FileSpacesToUnderscores.py
@@ -1,13 +1,12 @@
 import os
 
 # Define a folder to iterate through the files recursively
-folder_path = 'E:/Studying/Python Projects/PythonUtils/XML-files/'
+folder_path = 'dir_path'
 
 
 def empty_spaces(folder_path):
     for path, subdirs, files in os.walk(folder_path):
         for file_name in files:
-            print(file_name)
             file_path = os.path.join(path, file_name)
             new_name = os.path.join(path, file_name.replace(' ', '_'))
             os.rename(file_path, new_name)
